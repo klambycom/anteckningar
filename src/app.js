b87391cf10324f14a808612665a7a4cd0ccb0667
Christian Nilsson <christian@klamby.com>
2016-01-01 15:19:13 +0100

    Change layout

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import './app.scss';
 import React from 'react';
 import Sidebar from './sidebar';
 import Note from './note';
+import References from './references';
+
 import markdown from './markdown';
 
 let App = React.createClass({
@@ -21,6 +23,7 @@ let App = React.createClass({
         <div id='app'>
           <Sidebar headlines={mkd.headlines} links={mkd.internLinks} />
           <Note text={mkd.html} />
+          <References />
         </div>
         );
   }
---
+++ src/references.js
@@ -0,0 +1,17 @@
+import './references.scss';
+
+import React from 'react';
+
+let References = React.createClass({
+  // TODO Is footer wrong here?
+
+  render() {
+    return (
+        <footer id="references">
+          References
+        </footer>
+        );
+  }
+});
+
+export default References;
