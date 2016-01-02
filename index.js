0ee3dd4e70bc2f502b962915ca6aa87854f95106
Christian Nilsson <christian@klamby.com>
2016-01-02 13:26:55 +0100

    Add menu

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
 const electron = require('electron');
 const app = electron.app;
+
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 electron.crashReporter.start();
 
@@ -15,6 +17,142 @@ app.on('window-all-closed', function () {
 });
 
 app.on('ready', function () {
+  /*
+   * Menu
+   */
+
+  let menu = new Menu();
+
+  let template = [
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        },
+      ]
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          click: function (item, focusedWindow) {
+            if (focusedWindow) { focusedWindow.reload(); }
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: (function() {
+            if (process.platform == 'darwin') {
+              return 'Ctrl+Command+F';
+            }
+            else {
+              return 'F11';
+            }
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+            }
+          }
+        },
+      ]
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        },
+      ]
+    },
+    {
+      label: 'Help',
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: function() { require('electron').shell.openExternal('http://electron.atom.io') }
+        },
+      ]
+    },
+  ];
+
+  if (process.platform == 'darwin') {
+    let name = require('electron').app.getName();
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: 'About ' + name,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Services',
+          role: 'services',
+          submenu: []
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() { app.quit(); }
+        },
+      ]
+    });
+    // Window menu.
+    template[3].submenu.push(
+      { type: 'separator' },
+      { label: 'Bring All to Front', role: 'front' }
+    );
+  }
+
+  menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  /*
+   * Application
+   */
+
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
