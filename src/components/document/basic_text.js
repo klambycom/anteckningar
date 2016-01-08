112b7cbb60bbe14bef7737185aacdd30a243a10a
Christian Nilsson <christian@klamby.com>
2016-01-08 20:53:37 +0100

    Add basic text component

---
+++ src/components/document/basic_text.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// TODO Better name, this is not just basic text.
+let BasicText = React.createClass({
+  propTypes: {
+    children: React.PropTypes.array.isRequired
+  },
+
+  build(x, i) {
+    switch(x.type) {
+      case 'basic_text':
+        return x.text;
+
+      default:
+        return (
+            <span key={i} style={{color: 'red'}}>
+              The {x.type}-type is not supported in &gt;BasicText /&lt;!
+            </span>
+            );
+    }
+  },
+
+  render() {
+    return (<span>{this.props.children.map(this.build)}</span>);
+  }
+});
+
+export default BasicText;
